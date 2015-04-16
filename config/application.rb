92efde54851543d650ca963b49648200cf3157c0
simonasdev <simonasgildutis@gmail.com>
2015-04-17 00:34:54 +0300

    Specify database config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ database_path = File.expand_path("../database.yml", __FILE__)
 database_config = YAML.load(ERB.new(database_path).result)
 
 ENV["RACK_ENV"] ||= "development"
-ActiveRecord::Base.establish_connection(database_config)
+ActiveRecord::Base.establish_connection(database_config[ENV["RACK_ENV"]])
 
 # Order is important - helpers should go first
 %w(helpers api serializers models).each do |dir|
