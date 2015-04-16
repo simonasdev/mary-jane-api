d9182f3fc75fb1a6985274d5991347fc7a07d907
simonasdev <simonasgildutis@gmail.com>
2015-04-17 00:05:13 +0300

    Parse database file for embedded ruby

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
+require "erb"
 Bundler.require(:default)
 
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
 database_path = File.expand_path("../database.yml", __FILE__)
-database_config = YAML.load_file(database_path)
+database_config = YAML.load_file(ERB.new(database_path).result)
 
 ENV["RACK_ENV"] ||= "development"
 ActiveRecord::Base.establish_connection(database_config[ENV["RACK_ENV"]])
