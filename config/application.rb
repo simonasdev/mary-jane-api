bd94a7ed676f84465d5fcff4a9f74ac6d867ba65
simonasdev <simonasgildutis@gmail.com>
2015-04-17 00:08:49 +0300

    Try it out

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,16 @@
 require 'rubygems'
 require 'bundler/setup'
 require "erb"
+
 Bundler.require(:default)
 
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
 database_path = File.expand_path("../database.yml", __FILE__)
-database_config = YAML.load_file(ERB.new(database_path).result)
+database_config = YAML.load(ERB.new(database_path).result)
 
 ENV["RACK_ENV"] ||= "development"
-ActiveRecord::Base.establish_connection(database_config[ENV["RACK_ENV"]])
+ActiveRecord::Base.establish_connection(database_config)
 
 # Order is important - helpers should go first
 %w(helpers api serializers models).each do |dir|
