a3a5a62f179a2fc22454db205c10cbe0d11eabf3
simonasdev <simonasgildutis@gmail.com>
2015-04-18 13:16:54 +0300

    Change status of failed auth

--- lib/api/auth.rb
+++ lib/api/auth.rb
@@ -11,7 +11,7 @@ class Auth < Grape::API
       if @user && @user.authenticate(params[:password])
         { user: UserSerializer.new(@user), token: Token.encode(@user.id) }
       else
-        error!({ errors: ['Invalid credentials'] }, 500)
+        error!({ errors: ['Invalid credentials'] }, 401)
       end
     end
   end
