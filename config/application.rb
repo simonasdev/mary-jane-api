b94fff2322390246f3dda156929f3bf4e91983fa
simonasdev <simonasgildutis@gmail.com>
2015-07-18 03:01:55 +0300

    Log

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,11 @@ database_path = File.expand_path("../database.yml", __FILE__)
 database_config = YAML.load(ERB.new(database_path).result)
 
 RACK_ENV = ENV["RACK_ENV"] || "development"
+p RACK_ENV
+p database_config
 ActiveRecord::Base.establish_connection(RACK_ENV == 'production' ? database_config[RACK_ENV] : YAML.load_file(database_config)[RACK_ENV])
 
 # Order is important - helpers should go first
-%w(helpers api serializers models).each do |dir|
-  Dir[File.join(".", "lib/#{dir}/**/*.rb")].each {|_file| require _file}
+%w( helpers api serializers models ).each do |dir|
+  Dir[File.join(".", "lib/#{dir}/**/*.rb")].each { |_file| require _file }
 end
